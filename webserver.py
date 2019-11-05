e0c33834a4959d5016c1e7f6b473c149f468abe6
MenkeTechnologies <linux.dev25@gmail.com>
2019-11-04 19:27:22 -0500

    ajax get

--- webserver.py
+++ webserver.py
@@ -8,13 +8,22 @@
 ##### Notes: env FLASK_APP=hello.py flask run
 # }}}***********************************************************
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sys
 
 app = Flask("jarvis")
 
 @app.route('/')
 def webprint():
     return render_template('index.html')
+
+@app.route('/setcar')
+def setcar():
+    x = request.args.get("x")
+    y = request.args.get("y")
+    print(f"we got {x} and {y}")
+    return ""
+
 
 
 if __name__ == '__main__':
