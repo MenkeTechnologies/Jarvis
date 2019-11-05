10b82817b6969f4726644710582f3a59c1563072
MenkeTechnologies <linux.dev25@gmail.com>
2019-11-04 20:14:01 -0500

    poll

--- remoteRepoMonitorDaemon.sh
+++ remoteRepoMonitorDaemon.sh
@@ -30,13 +30,13 @@ main() {
     echo "killing python3 webserver.py"
     pkill python3
     echo "starting python3 webserver.py in background"
-    ( python3 webserver.py & >> $JARVIS_DIR/flask.log )
+    ( python3 $JARVIS_DIR/webserver.py & >> $JARVIS_DIR/flask.log )
 }
 
 echo "killing python3 webserver.py"
 pkill python3
 echo "starting python3 webserver.py in background"
-( python3 webserver.py & >> $JARVIS_DIR/flask.log )
+( python3 $JARVIS_DIR/webserver.py & >> $JARVIS_DIR/flask.log )
 
 while true; do
 
