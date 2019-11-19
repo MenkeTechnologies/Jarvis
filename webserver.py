795281bd3f36bd0f037d27df317b820290f75afd
Jacob Menke <jamenk@email.wm.edu>
2019-11-18 19:08:13 -0500

    global var

--- webserver.py
+++ webserver.py
@@ -18,26 +18,29 @@ app = Flask("jarvis")
 import time
 prev_time = time.time()
 
-
 @app.route('/')
 def webprint():
     return render_template('index.html')
 
 @app.route('/setcar')
 def setcar():
+    global prev_time
     cur_time = time.time()
     time_diff = cur_time - prev_time
 
-    if time_diff > 40:
+    if time_diff > .4:
+        prev_time = time.time()
+        print(f"time diff was {time_diff}")
         xpos = float(request.args.get("x"))
         ypos = float(request.args.get("y"))
         ESCset = 45*ypos+90
         turnSet =90*xpos+90
         data = [int(turnSet),int(ESCset)]
-        bus.write_i2c_block_data(address, 1,data)
+        # bus.write_i2c_block_data(address, 1,data)
         print(f"we got {xpos} and {ypos}")
     else:
-        print(f"time diff was {time_diff}")
+        pass
+        # print(f"time diff was {time_diff}")
     return ""
 
 
