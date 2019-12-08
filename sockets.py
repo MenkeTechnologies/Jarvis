8aae921b18a49a3bdcd5210d98e8272bf487eadd
MenkeTechnologies <linux.dev25@gmail.com>
2019-12-08 10:57:59 -0500

    writing every second 2 to address

--- sockets.py
+++ sockets.py
@@ -1,5 +1,6 @@
 import asyncio
 import sys
+import threading
 import time
 
 import smbus
@@ -13,9 +14,19 @@ channel = 1
 address = 0xa
 
 
+def run():
+    while True:
+        # print("writing to i2c")
+        time.sleep(1)
+        bus.write_byte(address, 2)
+
+
 async def hello(websocket, path):
     prev = time.time()
     print("connected")
+    thread = threading.Thread(target=run, args=())
+    thread.daemon = True
+    thread.start()
     while True:
         msg = await websocket.recv()
         now = time.time()
@@ -32,6 +43,7 @@ async def hello(websocket, path):
                 turnSet = 90 * float(x) + 90
                 data = [int(turnSet), int(ESCset)]
                 try:
+                    pass
                     bus.write_i2c_block_data(address, 1, data)
                 except:
                     print("could not write to i2c", file=sys.stderr)
